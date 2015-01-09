3ef48af84f3d376128e6fccc91a03913faff625e
mikael <guy.mikael@live.fr>
2015-01-09 12:16:42 +0100

    Creates Movie entity and repo bootstrap

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -9,8 +9,56 @@ use AppBundle\Entity\User;
 use AppBundle\Form\RegistrationType;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Security\Core\Security;
+
 class UserController extends Controller
 {
+    /**
+     * @Route("/login", name="login_route")
+     */
+    public function loginAction(Request $request) {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                Security::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $session->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(Security::LAST_USERNAME);
+
+        return $this->render(
+            'user/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+    );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+    }
+    
+    
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutCheckAction()
+    {
+    }
+    
     /**
      * @Route("/register", name="register")
      */
@@ -25,14 +73,13 @@ class UserController extends Controller
         
         // traite le formulaire
         $registrationForm->handleRequest( $request );
-        dump($user);
+        // dump($user);
         
         // si les données sont valides ...
         if ($registrationForm->isValid()) {
             // hydrate les autres proprietés de notre User
             
-                // hacher le mot de passe
-                
+            
                 //generer un salt
                 $salt = md5(uniqid());
                 $user->setSalt( $salt );
@@ -41,10 +88,25 @@ class UserController extends Controller
                 $token = md5(uniqid());
                 $user->setToken( $token );
                 
-                // les dates actuelles
                 
-            // sauvegarder le User en bdd
-            dump($user);
+                // hacher le mot de passe
+                // sha512, 5000 fois
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $user->getPassword());
+                $user->setPassword( $encoded );
+                
+                
+                // dates d'inscription et date de modification
+                $user->setDateRegistered(new \DateTime());
+                $user->setDateModified(new \DateTime());
+                
+                // assigne toujours ce role aux utilisateurs du front office
+                $user->setRoles( array("ROLE_USER"));
+                        
+                // sauvegarder le User en bdd
+                $em = $this->getDoctrine()->getManager();
+                $em->persist( $user );
+                $em->flush();
             
         }
         // on shoot le formulaire à Twig (on oublie pas le create view)
@@ -52,7 +114,6 @@ class UserController extends Controller
             "registrationForm" => $registrationForm->createView()
             
         );
-        
         return $this->render('user/register.html.twig', $params);
     }
 }
